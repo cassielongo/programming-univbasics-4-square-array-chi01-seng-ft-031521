9ba9bd93b76025687607fae85bc09d4479d56f60
mathematical-language-4640 <longocassie@yahoo.com>
2021-03-06 05:10:28 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,7 @@
 def square_array(array)
-  array_length = array.length 
-  squared_array = []
-  array_length.times do |index|
-    new_element = array[index]**2
-    squared_array.push new_element
-  end 
-  squared_array
+ new_array = []
+  array.length.times do |index|
+  new_array.push(array[index] ** 2)
+  end
+ new_array
 end
